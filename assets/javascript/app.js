2e5c1ff68ad4247b1b83289bfabd07834ab4b1ff
Lea Abetya <leaabetya@Leas-MacBook-Air-2.local>
2017-08-15 22:11:52 -0600

    game ending correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,25 +19,166 @@ $(document).ready(function() {
 
 	var showQuestion;
 	var startTime;
-	var count = 0;
+	var count;
 	var correct = 0;
 	var time = 10;
-	var clockRunning = false;
+	var clockRunning = true;
+	var testImage = "https://images-na.ssl-images-amazon.com/images/M/MV5BZjZjMzQ2ZmUtZWEyZC00NWJiLWFjM2UtMzhmYzZmZDcxMzllXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UY268_CR3,0,182,268_AL_.jpg";
 
 	$('#startButton').click(function () {
-		displayQuestion();
-		startGame();
+		showQuestion = setInterval(nextQuestion, 10000);
+	    startTime = setInterval(timer, 1000);
+	    // clockRunning = true;
+		// startGame();
 		$('#startButton').hide();
 		timer();
+		count = 0;
+		displayQuestion();
 	});
 
 
+	function displayQuestion() {
+
+	  var choiceDiv = $('#choice-list');
+
+	  $('#question').html(questionArray[count].question);
+	  
+	  for (var i = 0; i < questionArray[count].choices.length; i++) {
+	  	var choice = $("<li>" + questionArray[count].choices[i] + "</li>" + "<br />");
+	  	choice.attr('data-value', questionArray[count].choices[i]);
+	  	
+	  	choice.on('click', function () {
+	  		console.log($(this).attr('data-value'));
+	  		console.log(questionArray[count].answer);
+
+	  		time = 10;
+
+
+	  		if ($(this).attr('data-value') === questionArray[count].answer) {
+	  			
+	  			correct++;
+	  			// $('#choice-list').hide();
+	  			// $('#image').attr('src', testImage);
+	  			// setTimeout(hideImage, 5000);
+	  			// setTimeout(nextQuestion, 5000);
+	  			// clearInterval(showQuestion);
+	  			// clockRunning = false;
+	  			// clearInterval(timer);
+
+	  			nextQuestion();
+	  			
+	  		} else {
+	  			
+	  			$('#choice-list').hide();
+	  			$('#image').attr('src', testImage);
+	  			setTimeout(hideImage, 5000);
+	  			setTimeout(nextQuestion, 5000);
+	  			
+	  		}
+
+	  		clearInterval(showQuestion);
+	  		showQuestion = setInterval(nextQuestion, 10000);
+	  		
+	  	});
+
+	  	choiceDiv.append(choice);
+
+	  }
+
+	}
+
+	function nextQuestion() {
+	  
+	  count++
+	  console.log(count)
+	  // showQuestion = setInterval(nextQuestion, 10000);
+
+	  time = 10
+
+	  $('#choice-list').empty();
+	  // $('#choice-list').show();
+
+	  var newList = $('#choice-list');
+
+		// var l = questionArray[count] ? questionArray[count].choices.length : 0;
+
+		if (questionArray[count]) {
+
+		  $('#question').html(questionArray[count].question);
+
+		  for (var i = 0; i < questionArray[count].choices.length; i++) {
+		  	var choice = $("<li>" + questionArray[count].choices[i] + "</li>" + "<br />");
+		  	choice.attr('data-value', questionArray[count].choices[i]);
+
+		  	choice.on('click', function () {
+		  		console.log($(this).attr('data-value'));
+		  		console.log(questionArray[count].answer);
+
+		  		// time = 10;
+
+		  		if ($(this).attr('data-value') === questionArray[count].answer) {
+		  			
+		  			correct++;
+		  			// $('#choice-list').hide();
+		  			nextQuestion();
+		  			console.log('this is correct ' + correct);
+		  			
+		  		} else {
+		  			
+		  			nextQuestion();
+		  			// alert('wrong!');
+		  			
+		  		}
+
+		  		if (questionArray[count]) { 
+
+			  		clearInterval(showQuestion);
+			  		console.log('inside function ' + showQuestion);
+			  		showQuestion = setInterval(nextQuestion, 10000);
+
+			  	} else {
+			  		clearInterval(showQuestion);
+			  	}
+		  		
+		  	});	    
+
+		  	$('#choice-list').append(choice);
+		  	
+		  }
+
+		} else {
+			
+			resetGame();	
+				
+		}	 
+
+	}
+
+	function resetGame() {
+      
+      $('#question').html("");
+
+	  		alert(`You got ${correct} out of 5`);
+	  		time = 10;
+	 	    // clockRunning = false;
+	  		correct = 0;
+	  		$('#startButton').show();
+	  		clearInterval(showQuestion);
+	  		clearInterval(startTime);
+	  		$('#time').html("");
+	  		console.log('reset game');
+	  		console.log('inside resetGame ' + showQuestion);
+
+	}
+
 	function timer() {
-		time--;
 
 		var converted = timeConverter(time);
-		console.log(converted);
+		
 		$('#time').html(converted);
+
+		time--;
+	  
 	}
 
 	function timeConverter(t) {
@@ -56,113 +197,17 @@ $(document).ready(function() {
 	  return minutes + ":" + seconds;
 	}
 
-	function displayQuestion() {
-
-	  var choiceDiv = $('#choice-list');
-
-	  $('#question').html(questionArray[count].question);
-	  
-	  for (var i = 0; i < questionArray[count].choices.length; i++) {
-	  	var choice = $("<li>" + questionArray[count].choices[i] + "</li>" + "<br />");
-	  	// choice.val(questionArray[count].choices[i]);
-	  	
-	  	choice.bind('click', function () {
-	  		console.log($(this).html());
-	  		console.log(questionArray[count].answer);
-
-	  		time = 10;
-
-	  		if ($(this).html() === questionArray[count].answer) {
-	  			
-	  			nextQuestion();
-	  			alert('correct!');
-	  			correct++;
-	  			
-	  		} else {
-	  			
-	  			nextQuestion();
-	  			alert('wrong!');
-	  			
-	  		}
-
-	  		clearInterval(showQuestion);
-	  		showQuestion = setInterval(nextQuestion, 10000);
-	  	});
-
-	  	choiceDiv.append(choice);
-	  	// console.log(choice);
-
-
-	  }
-
-	}
-
-	function nextQuestion() {
-	  
-	  count++
-
-	  time = 10
-
-	  $('#choice-list').empty()
-
-	  $('#question').html(questionArray[count].question);
-
-	  var newList = $('#choice-list')
-
-	  for (var i = 0; i < questionArray[count].choices.length; i++) {
-	  	var choice = $("<li>" + questionArray[count].choices[i] + "</li>" + "<br />");
-	  	// choice.val(questionArray[count].choices[i]);
-	  	choice.bind('click', function () {
-	  		console.log($(this).html());
-	  		console.log(questionArray[count].answer);
-
-	  		time = 10;
-
-	  		if ($(this).html() === questionArray[count].answer) {
-	  			
-	  			nextQuestion();
-	  			alert('correct!');
-	  			correct++;
-	  			
-	  		} else {
-	  			
-	  			nextQuestion();
-	  			alert('wrong!');
-	  			
-	  		}
-
-	  		clearInterval(showQuestion);
-	  		showQuestion = setInterval(nextQuestion, 10000);
-	  	});
-	  	$('#choice-list').append(choice);
-	  	// console.log(choice);
-	  }
-
-	  if (count === questionArray.length) {
-	  	  
-	  	  stopGame();
-	  	  alert(`You got ${correct} out of 5`);
-	  	  // count = 0;
-	  }
-
-	}
-
-	function startGame() {
-	  if (!clockRunning) {
-	
-	    showQuestion = setInterval(nextQuestion, 10000);
-	    startTime = setInterval(timer, 1000);
-	    clockRunning = true;
-	  }
-	}
-
-	function stopGame() {
-
-	  clearInterval(showQuestion);
-
-	}
-
 });
+
+function hideImage() {
+	$('#image').hide();
+}
+
+function resetInterval() {
+	setInterval(nextQuestion, 10000)
+}
+
+
 
 
 
